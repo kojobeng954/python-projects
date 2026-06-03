9a1427840b3f1d02da5231854b97338118bd94b3
kojobeng954 <kojobeng954@gmail.com>
2026-06-03 00:25:49 +0000

    First trial with if statement

---
+++ good-credit.py
@@ -0,0 +1,9 @@
+price = 1000000
+good_credit = True
+
+
+if good_credit:
+    down_payment = int(0.1 * price)
+else:
+    down_payment = int(0.1 * price)
+print(f"Your down payment is {down_payment}USD")
