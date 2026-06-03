3d12e91075a0a7848943e3fbf51eea6d5de00940
kojobeng954 <kojobeng954@gmail.com>
2026-06-03 00:23:13 +0000

    add even or odd checker - modulo operator and nested if statements

---
+++ modulo.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number to know if it is odd or even?: "))
+number = num % 2
+
+if number == 0:
+    print("This is an even number.")
+else:
+    print("This is an odd number")
