d7037d3d0d038e4614ffca0dfd9548bd341aa723
kojobeng954 <kojobeng954@gmail.com>
2026-06-04 00:44:35 +0000

    add celsius to fahrenheit converter - arithmetic and operator precedence

---
+++ temperature-converter.py
@@ -0,0 +1,5 @@
+temp = int(input("What is your temperature?: "))
+new_temp = (temp * (9 / 5)) + 32
+print(f"Your temperature is {new_temp} Farenheit")
+
+# add celsius to fahrenheit converter - arithmetic and operator precedence
