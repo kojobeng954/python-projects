8da7c48a567de8292247539baab8c1198019a60b
kojobeng954 <kojobeng954@gmail.com>
2026-06-04 00:46:19 +0000

    add password strength checker - if statements and string length

---
+++ password_checker.py
@@ -0,0 +1,7 @@
+password = input("What is your password?: ")
+if len(password) > 8:
+    print("This is a strong password.")
+else:
+    print("This is a weak password.")
+
+# add password strength checker - if statements and string length
